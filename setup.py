c61dba4f936579b4e47228ba2c7144dfc4bebdc8
kitfactory <kitfactory@gmail.com>
2019-02-11 17:43:35 +0900

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ermine",
+    version="0.1",
+    author="kitfactory",
+    author_email="kitfactory@gmail.com",
+    description="libhollow is my own python package",
+    long_description="The halloween party has gone over...",
+    long_description_content_type="text/markdown",
+    url="https://blog.chocolapod.net/momokan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6.0",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
